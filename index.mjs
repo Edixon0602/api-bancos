523f39d913a8877dabc019c28913a7051ab47849
Edixon Serrano <e.serrano0602@gmail.com>
2024-05-22 11:19:08 -0400

    Add: cors middleware

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
 import express from 'express'
 import indexRouter from './routes/index.mjs'
+import cors from 'cors'
 const app = express()
 const port = process.env.port || 3000
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
